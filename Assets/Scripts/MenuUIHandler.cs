fa90e21020b9b22a14fc7adf6a11f098c00901e4
David Liu <liudav@microsoft.com>
2022-09-02 12:46:11 -0700

    created menu
    created menu scene and added buttons to enable start of game and to quit

---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// Sets the script to be executed later than all default scripts
+// This is helpful for UI, since other things may need to be initialized before setting the UI
+[DefaultExecutionOrder(1000)]
+public class MenuUIHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Quit()
+    {
+        #if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+        #endif
+        Application.Quit(); // original code to quit Unity player
+    }
+}
